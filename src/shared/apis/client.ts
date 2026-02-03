112c4b58be356ae10a4477624d1db80736cf0f77
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-02-03 12:01:16 +0900

    init: axiosInstance 설정

---
+++ src/shared/apis/client.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const instance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+export const get = async <T>(...args: Parameters<typeof instance.get>) => {
+  const response = await instance.get<T>(...args);
+  return response.data;
+};
+
+export const post = async <T>(...args: Parameters<typeof instance.post>) => {
+  const response = await instance.post<T>(...args);
+  return response.data;
+};
