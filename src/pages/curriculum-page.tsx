72a0ce79f536c3c1e4d9a97be6a2485a0f402670
tama <rufina130@naver.com>
2026-03-15 16:22:58 +0900

    [fix] Prettier 적용

--- src/pages/curriculum-page.tsx
+++ src/pages/curriculum-page.tsx
@@ -3,10 +3,10 @@ import CurriculumBox from '@/shared/components/curriculum-box/curriculum-box';
 
 // 나중에 백엔드 API에서 받을 데이터의 예상 구조
 const CURRICULUM_DATA = [
-  { id: 1, text: "딥러닝 기초 : ANN, DNN, CNN" },
-  { id: 2, text: "딥러닝 기초 : ANN, DNN, CNN" },
-  { id: 3, text: "딥러닝 기초 : ANN, DNN, CNN" },
-  { id: 4, text: "딥러닝 기초 : ANN, DNN, CNN" },
+  { id: 1, text: '딥러닝 기초 : ANN, DNN, CNN' },
+  { id: 2, text: '딥러닝 기초 : ANN, DNN, CNN' },
+  { id: 3, text: '딥러닝 기초 : ANN, DNN, CNN' },
+  { id: 4, text: '딥러닝 기초 : ANN, DNN, CNN' },
 ];
 
 const CurriculumPage = () => {
