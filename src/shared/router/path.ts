06f61923213c172a419aceceddf15404d4903e49
JI SUN AHN <rufina130@naver.com>
2026-03-16 12:05:40 +0900

    Revert "curriculum-box 개발"

--- src/shared/router/path.ts
+++ src/shared/router/path.ts
@@ -1,7 +1,6 @@
 export const ROUTE_PATH = {
   HOME: '/',
   // 페이지 추가
-  CURRICULUM: '/curriculum',
 } as const;
 
 export type Routes = (typeof ROUTE_PATH)[keyof typeof ROUTE_PATH];
--- src/shared/router/router.tsx
+++ src/shared/router/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router';
 
-import CurriculumPage from '@/pages/curriculum-page';
 // TODO: 에러 페이지 추가 예정
 import { HomePage } from '@/shared/router/lazy';
 import { ROUTE_PATH } from '@/shared/router/path';
@@ -9,9 +8,5 @@ export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.HOME,
     Component: HomePage,
-  },
-  {
-    path: ROUTE_PATH.CURRICULUM,
-    Component: CurriculumPage,
   },
 ]);
