b7b3719dfbba6270a2eb018934610610bbd81d1d
tama <rufina130@naver.com>
2026-03-16 19:35:42 +0900

    fix: review-card 수정 중

--- src/shared/router/router.tsx
+++ src/shared/router/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router';
 
 // TODO: 에러 페이지 추가 예정
-import { CurriculumPage, HomePage } from '@/shared/router/lazy';
+import { HomePage } from '@/shared/router/lazy';
 import { ROUTE_PATH } from '@/shared/router/path';
 
 export const router = createBrowserRouter([
