74b4bc5927948095fd551c8a6f5127cfc811b7a1
Yoonji Kim <161998554+yooncandooit@users.noreply.github.com>
2026-03-18 00:56:13 +0900

    [Feat] Progress Bar 컴포넌트 UI (#34)
    * feat: progress bar 컴포넌트 UI 구현
    
    * refactor: section 컴포넌트에서 상태 제거
    
    * del: 테스트 코드 제거
    
    * chore: 고정 width 값 제거

---
+++ src/shared/components/progress-bar/progress-bar.css.ts
@@ -0,0 +1,22 @@
+import { recipe } from '@vanilla-extract/recipes';
+
+import { themeVars } from '@/shared/styles/theme.css';
+
+export const progressBar = recipe({
+  base: {
+    width: '100%',
+    height: '1rem',
+    border: `1px solid ${themeVars.color.grayscale[800]}`,
+    borderRadius: '20px',
+    backgroundColor: 'transparent',
+  },
+  variants: {
+    isCurrentStep: {
+      true: {
+        backgroundColor: `${themeVars.color.grayscale[100]}`,
+        transition: 'background-color 0.1s ease',
+      },
+      false: {},
+    },
+  },
+});
