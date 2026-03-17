fd040fe97366adcb83a401bc3d2c6f434447c71e
tama <rufina130@naver.com>
2026-03-18 01:14:06 +0900

    fix: router.tsx 수정

--- src/shared/router/router.tsx
+++ src/shared/router/router.tsx
@@ -1,40 +1,16 @@
-import { createBrowserRouter, Outlet } from 'react-router';
+import { createBrowserRouter } from 'react-router';
 
-import {
-  ArchivePage,
-  CurriculumPage,
-  FAQPage,
-  HomePage,
-  RecruitingPage,
-} from '@/shared/router/lazy';
+// TODO: 에러 페이지 추가 예정
+import { CurriculumPage, HomePage } from '@/shared/router/lazy';
 import { ROUTE_PATH } from '@/shared/router/path';
 
 export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.HOME,
-
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-    ],
-    // Component: HomePage,
+    Component: HomePage,
   },
   {
     path: ROUTE_PATH.CURRICULUM,
     Component: CurriculumPage,
-  },
-  {
-    path: ROUTE_PATH.RECRUITING,
-    Component: RecruitingPage,
-  },
-  {
-    path: ROUTE_PATH.ARCHIVE,
-    Component: ArchivePage,
-  },
-  {
-    path: ROUTE_PATH.FAQ,
-    Component: FAQPage,
   },
 ]);
