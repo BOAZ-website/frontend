5b82288151da9f9ffbd66ff539ac717d7d5b1145
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-20 04:54:24 +0900

    chore: 테스트 코드 삭제

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,11 +1,5 @@
-import SectionHeader from '@/shared/components/section-header/section-header';
-
 const HomePage = () => {
-  return (
-    <div style={{ padding: '2rem', backgroundColor: '#000' }}>
-      <SectionHeader subTitle="Welcome to our site" title="Home" />
-    </div>
-  );
+  return <></>;
 };
 
 export default HomePage;
