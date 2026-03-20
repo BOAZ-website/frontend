4978fc0ed94eeca246917cd457af3dbfa583cdd0
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-20 21:48:09 +0900

    chore: description 타입 수정

--- src/widgets/review-card-list/review-card.tsx
+++ src/widgets/review-card-list/review-card.tsx
@@ -1,12 +1,10 @@
-import type { ReactNode } from 'react';
-
 import * as styles from './review-card.css';
 
 interface ReviewCardProps {
   title: string;
   generation: string;
   track: string;
-  description: ReactNode;
+  description: string;
 }
 
 const ReviewCard = ({ title, generation, track, description }: ReviewCardProps) => {
