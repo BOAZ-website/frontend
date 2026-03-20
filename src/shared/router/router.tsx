4efd1f9863dc48e29c4d4706e74afac5d3ec429a
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-20 16:12:19 +0900

    feat: 페이지 라우터 추가

--- src/shared/router/router.tsx
+++ src/shared/router/router.tsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from 'react-router';
 
 // TODO: 에러 페이지 추가 예정
-import { CurriculumPage, HomePage } from '@/shared/router/lazy';
+import {
+  ArchivePage,
+  CurriculumPage,
+  FAQPage,
+  HomePage,
+  RecruitingPage,
+} from '@/shared/router/lazy';
 import { ROUTE_PATH } from '@/shared/router/path';
 
 export const router = createBrowserRouter([
@@ -12,5 +18,17 @@ export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.CURRICULUM,
     Component: CurriculumPage,
+  },
+  {
+    path: ROUTE_PATH.RECRUITING,
+    Component: RecruitingPage,
+  },
+  {
+    path: ROUTE_PATH.ARCHIVE,
+    Component: ArchivePage,
+  },
+  {
+    path: ROUTE_PATH.FAQ,
+    Component: FAQPage,
   },
 ]);
