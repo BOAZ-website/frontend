a224c6a1d2507c3a42f90048b759284d7240ed7d
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-23 01:13:00 +0900

    style: 가운데 정렬 삭제

--- src/shared/components/section/section.css.ts
+++ src/shared/components/section/section.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
+  alignItems: 'stretch',
+  width: '100%',
   gap: '8rem',
 });
