04a73e2fd5613850f3f333058830e8a3c0157fd0
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-23 13:04:07 +0900

    feat: pagination 컴포넌트 구현

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
+import Pagination from '@/shared/components/pagination/pagination';
+
+const MOCK_TOTAL_PAGES = 10;
+
 const HomePage = () => {
-  return <>HomePage</>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // TODO: API 연동 후 삭제
+  const hasPrevious = currentPage > 1;
+  const hasNext = currentPage < MOCK_TOTAL_PAGES;
+
+  return (
+    <>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={MOCK_TOTAL_PAGES}
+        hasPrevious={hasPrevious}
+        hasNext={hasNext}
+        onPageChange={setCurrentPage}
+      />
+    </>
+  );
 };
 
 export default HomePage;
