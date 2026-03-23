d7f568e130ae61d3001dd528cdf2f8336c45cfed
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-23 13:20:19 +0900

    chore: 불필요 prop 삭제

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -7,17 +7,11 @@ const MOCK_TOTAL_PAGES = 10;
 const HomePage = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
-  // TODO: API 연동 후 삭제
-  const hasPrevious = currentPage > 1;
-  const hasNext = currentPage < MOCK_TOTAL_PAGES;
-
   return (
     <>
       <Pagination
         currentPage={currentPage}
         totalPages={MOCK_TOTAL_PAGES}
-        hasPrevious={hasPrevious}
-        hasNext={hasNext}
         onPageChange={setCurrentPage}
       />
     </>
