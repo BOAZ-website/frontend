f5c40cfcae4a0f5d8c67ea02949c2668a9fa2c40
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-03-24 13:22:03 +0900

    del: 테스트 제거

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,21 +1,5 @@
-import { useState } from 'react';
-
-import Pagination from '@/shared/components/pagination/pagination';
-
-const MOCK_TOTAL_PAGES = 10;
-
 const HomePage = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  return (
-    <>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={MOCK_TOTAL_PAGES}
-        onPageChange={setCurrentPage}
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default HomePage;
