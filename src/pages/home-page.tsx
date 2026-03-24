31645301e3ca153f345da1e7eb09b327f11aca58
Yoonji Kim <161998554+yooncandooit@users.noreply.github.com>
2026-03-24 13:25:18 +0900

    [Feat] Pagination 컴포넌트 구현 (#67)
    * feat: pagination 컴포넌트 구현
    
    * chore: 불필요 prop 삭제
    
    * del: 테스트 제거
    
    * add: 접근성 라벨 aria-label 추가

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,11 +1,5 @@
-import TextFieldWithCounter from '@/shared/components/textfield-with-counter/textfield-with-counter';
-
 const HomePage = () => {
-  return (
-    <div style={{ width: '300px' }}>
-      <TextFieldWithCounter maxLength={1} placeholder="Enter text..." />
-    </div>
-  );
+  return <></>;
 };
 
 export default HomePage;
