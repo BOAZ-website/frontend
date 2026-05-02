91aa6b3f29c6401b288bc72c54915eb35e1a98cd
Yoonji Kim <dotsi0@dgu.ac.kr>
2026-05-03 06:41:42 +0900

    chore: 리크루팅 타이포 수정

--- src/widgets/recruiting/model/value-items.ts
+++ src/widgets/recruiting/model/value-items.ts
@@ -8,13 +8,13 @@ export const VALUE_ITEMS = [
   {
     id: 2,
     icon: '',
-    title: '같이의 가치',
-    description: '함께 성장하는 보아즈',
+    title: '깊이의 품격',
+    description: '체계적인 배움과 탐구',
   },
   {
     id: 3,
     icon: '',
-    title: '같이의 가치',
-    description: '함께 성장하는 보아즈',
+    title: '도전의 증명',
+    description: '과감한 도전과 성장',
   },
 ];
