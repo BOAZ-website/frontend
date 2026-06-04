50fda8741246c4639adbb1eac9e0ee2574e15ca4
ity0526 <ity0526@gmail.com>
2026-06-04 22:14:25 +0900

    feat: 우측 브라우저 스크롤 바 삭제

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,17 +1,26 @@
+import { useEffect } from 'react';
+
 const HomePage = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    document.body.style.scrollbarWidth = 'none';
+    return () => {
+      document.body.style.overflow = '';
+      document.body.style.scrollbarWidth = '';
+    };
+  }, []);
+
   return (
-    <>
-      <iframe
-        src="/landing.html"
-        style={{
-          width: '100%',
-          height: 'calc(100vh - 50px)',
-          border: 'none',
-          display: 'block',
-          overflow: 'auto',
-        }}
-      />
-    </>
+    <iframe
+      src="/landing.html"
+      style={{
+        width: '100%',
+        height: 'calc(100vh - 60px)',
+        border: 'none',
+        display: 'block',
+        overflow: 'auto',
+      }}
+    />
   );
 };
 
