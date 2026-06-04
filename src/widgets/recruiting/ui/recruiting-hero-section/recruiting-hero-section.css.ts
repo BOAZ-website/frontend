8d9abfbbd223e37e2a098010991e2efffebca4a2
ity0526 <ity0526@gmail.com>
2026-06-05 02:32:51 +0900

    style: 리쿠르팅 베너 섹션 컨테이너 생성 및 문구 추가

--- src/widgets/recruiting/ui/recruiting-hero-section/recruiting-hero-section.css.ts
+++ src/widgets/recruiting/ui/recruiting-hero-section/recruiting-hero-section.css.ts
@@ -1,7 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-import { media } from '@/shared/styles/breakpoints';
-
 import { themeVars } from '@/shared/styles/theme.css';
 import { typography } from '@/shared/styles/typography.css';
 
@@ -9,41 +7,23 @@ export const container = style({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  color: `color-mix(in srgb, ${themeVars.color.grayscale.white} 30%, transparent)`,
-});
-
-export const subTitle = style({
-  ...typography.h3_sb_30,
-  marginBottom: '2.5rem',
-  '@media': {
-    [media.mobile]: {
-      ...typography.body2_bd_20,
-      marginBottom: '2rem',
-    },
-  },
+  justifyContent: 'center',
+  width: '100%',
+  height: '80vh',
+  gap: '2.4rem',
+  textAlign: 'center',
+  paddingBottom: '10rem',
 });
 
 export const title = style({
-  ...typography.display1_bd_80,
-  marginBottom: '5rem',
-  '@media': {
-    [media.mobile]: {
-      ...typography.h1_bd_36,
-      marginBottom: '2.8rem',
-    },
-  },
-});
-
-export const emphasis = style({
-  color: `color-mix(in srgb, ${themeVars.color.primary[100]} 30%, transparent)`,
-  fontStyle: 'normal',
+  ...typography.display2_bd_72,
+  color: themeVars.color.grayscale.white,
 });
 
 export const description = style({
-  ...typography.h1_md_40,
-  '@media': {
-    [media.mobile]: {
-      ...typography.h4_bd_24,
-    },
-  },
+  ...typography.body4_rg_18,
+  color: themeVars.color.grayscale.white,
+  marginTop: '2rem',
+  lineHeight: '1.6',
+  marginBottom: '2rem',
 });
